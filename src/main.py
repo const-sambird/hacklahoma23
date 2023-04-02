7acb01da0e3755136076bbbed8631ea2fc34292c
josh lee <joshuaree138@gmail.com>
2023-04-02 04:21:23 -0500

    backgrounds ( needs resizing)

--- src/main.py
+++ src/main.py
@@ -8,14 +8,25 @@ pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
+backgrounds = pygame.image.load("../assets/backgrounds/devon floor 1.jpg")
+# Get the image dimensions
+image_width, image_height = backgrounds.get_size()
+
+# Calculate the x and y coordinates for the image to be centered on the screen
+x = (screen_width - image_width) / 2
+y = (screen_height - image_height) / 2
+
 
 levels = [level_map_1, level_map_2, level_map_3, level_map_4, level_map_5]
-level_index = 1
+level_index = 5
 
 lives = 3
 level = Level(levels[level_index - 1], screen, level_index, lives)
 
-background_image = pygame.image.load("../assets/backgrounds/902358-pixel-art-artwork-city-sunrise-skyline-cityscape.png").convert()
+
+# Dictionary of level numbers and corresponding background image filenames
+backgrounds = {1: "../assets/backgrounds/devon floor 1.jpg", 2: "../assets/backgrounds/devon floor 2.jpg", 3: "../assets/backgrounds/devon floor 3.jpg", 4: "../assets/backgrounds/devon floor 4.jpg", 5: "../assets/backgrounds/902358-pixel-art-artwork-city-sunrise-skyline-cityscape.png"}
+background_img = pygame.image.load(backgrounds[1])
 
 while True:
     for event in pygame.event.get():
@@ -23,10 +34,10 @@ while True:
             pygame.quit()
             sys.exit()
     
-    screen.blit(background_image, (0, 0))
-    #screen.fill('black')
+    
+    screen.blit(background_img, (x,y))
     level.run()
-
+    
     pygame.display.update()
     clock.tick(60)
 
@@ -48,8 +59,10 @@ while True:
         if (level_index <= 5):
             level_index += 1
             level = Level(levels[level_index - 1], screen, level_index, lives)
-            #create array of backgrounds
-            #update background based on level_index
+            # Load background image based on current level number
+            background_img = pygame.image.load(backgrounds[level_index])
+            screen.blit(background_img, (x,y))
+            pygame.display.update()
             
     
     if level.dead:
