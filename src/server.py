a4612017aa1cbcee0b8f9e3a7c4ee1ba1546f060
Anthony Nguyen <anthonynguyenlaas@gmail.com>
2023-04-02 08:08:22 -0500

    post requests? again lol

--- src/server.py
+++ src/server.py
@@ -1,13 +1,18 @@
-from flask import Flask
-from flask import request
+import requests
 
-app = Flask(__name__)
+def sendScore(ticks_data):
+    requests.post("https://nguyentendo.sambird.dev/api/score", data = ticks_data)
 
-@app.route('/newScore', methods=['GET', 'POST'])
-def sendScore(tick_data):
-    if request.method == 'POST':
-        return tick_data
-
-
-if __name__ == '__main__':
-   app.run(debug = True)
+# from flask import Flask
+# from flask import request
+#
+# app = Flask(__name__)
+#
+# @app.route('/newScore', methods=['GET', 'POST'])
+# def sendScore(tick_data):
+#     if request.method == 'POST':
+#         return tick_data
+#
+#
+# if __name__ == '__main__':
+#    app.run(debug = True)
