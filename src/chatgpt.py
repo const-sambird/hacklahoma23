470778db3be14a02a34857315adfc8fc6b41d173
Sam Bird <sam@sambird.dev>
2023-04-02 00:41:18 -0500

    chatgpt sprite

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -3,8 +3,7 @@ import pygame
 class Chatgpt(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
-        self.image = pygame.Surface((size, size))
-        self.image.fill('yellow')
+        self.image = pygame.image.load('../assets/tiles/chatgpt.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
     
     def update(self, x_shift):
