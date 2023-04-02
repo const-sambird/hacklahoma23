474931e12edcc680206e6e00b97c65ef5e4744f1
Sam Bird <sam@sambird.dev>
2023-04-02 02:53:32 -0500

    game over

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ clock = pygame.time.Clock()
 levels = [level_map_1, level_map_2, level_map_3, level_map_4, level_map_5]
 level_index = 5
 
-lives = 3
+lives = 1
 level = Level(levels[level_index - 1], screen, level_index, lives)
 
 background_image = pygame.image.load("../assets/backgrounds/902358-pixel-art-artwork-city-sunrise-skyline-cityscape.png").convert()
@@ -56,3 +56,18 @@ while True:
         lives -= 1
         level_index = 1
         level = Level(levels[level_index - 1], screen, level_index, lives)
+        if lives < 1:
+            break
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill('black')
+    font = pygame.font.Font('../assets/font.ttf', 64)
+    game_over = font.render("Game Over", False, 'white')
+    w, h = font.size("Game Over")
+    screen.blit(game_over, ((screen_width - w) // 2, (screen_height - h) // 2))
+    pygame.display.update()
+    clock.tick(60)
