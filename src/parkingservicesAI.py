7555d10f8652676a705d03588c96897b9c2b9077
Sam Bird <sam@sambird.dev>
2023-04-02 03:57:52 -0500

    parking services sprites

--- src/parkingservicesAI.py
+++ src/parkingservicesAI.py
@@ -1,12 +1,42 @@
 import pygame
+from util import import_folder
 
 class ParkingServicesAI(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
-        self.pos = pos
-        self.image = pygame.Surface((size, size))
-        self.image.fill('magenta')
-        self.rect = self.image.get_rect(topleft = pos)
+        self.import_character_assets()
+        self.frame_index = 0
+        self.animation_speed = 0.05
+        self.image = self.animations[self.frame_index]
+        self.rect = self.image.get_rect(midtop = pos)
+        
+         # movement
+        
+        # functionality 
+        self.here = 1 # checks if the ticket has been killed
+
+        # appearance
+        self.facing_right = True
+        self.on_ground = False
+        self.on_ceiling = False
+        self.on_left = False
+        self.on_right = False
+    
+    def import_character_assets(self):
+        path = '../assets/parking/'
+        self.animations = import_folder(path)
+
+    def animate(self):
+        animation = self.animations
+
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+        
+        image = animation[int(self.frame_index)]
+        self.rect = self.image.get_rect(center = self.rect.center)
+        self.image = image
     
     def update(self, x_shift):
         self.rect.x += x_shift
+        self.animate()
